9155fb39e59ff184969df562567b0599fd07ce57
Florian Alu <fla@grapps.fr>
2026-07-14 13:47:21 +0200

    switch to case system

--- nobinobi_daily_follow_up/templatetags/class_table_tags.py
+++ nobinobi_daily_follow_up/templatetags/class_table_tags.py
@@ -3,12 +3,15 @@ register = template.Library()
 
 @register.simple_tag
 def get_period_class(period, classroom):
-    if period.absence: return "bg-danger-gradient"
-    if period.status == "present": return "bg-present"
-    if period.status == "leave" and period.troubleshooting: return "bg-troubleshooting-gradient"
-    if period.status == "leave": return "bg-leave-gradient"
-    if period.replacement_classroom == classroom: return "bg-replacement-classroom"
-    if period.status == "troubleshooting": return "bg-troubleshooting"
-    if period.status == "replacement_classroom": return "bg-dark-gradient"
-    if period.status == "expected": return "bg-warning-gradient"
-    return "bg-gray"
+    match period.status:
+        case "absence": return "bg-danger-gradient"
+        case "present": return "bg-present"
+        case "leave":
+            if period.troubleshooting: return "bg-troubleshooting-gradient"
+            return "bg-leave-gradient"
+        case "troubleshooting": return "bg-troubleshooting"
+        case "replacement_classroom":
+            if period.replacement_classroom and period.replacement_classroom.id == classroom.id: return "bg-replacement-classroom"
+            return "bg-dark-gradient"
+        case "expected": return "bg-warning-gradient"
+        case _: return "bg-gray"
